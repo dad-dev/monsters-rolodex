e1cbfaa593018a7e42ab549b2e8b596cf1f72b16
Jeff Hall <hall.jeffw@gmail.com>
2020-07-03 10:04:45 -0400

    Updates

---
+++ precache-manifest.df40f46bb4069b894ef9f44db1425757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2502c63bc1b456539099357cb4c530",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9dce1a01f1776968299a",
+    "url": "/monsters-rolodex/static/css/main.6276f102.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dce1a01f1776968299a",
+    "url": "/monsters-rolodex/static/js/main.15ca463a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
